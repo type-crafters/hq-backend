9b7e74a6eed9f7538156fca9949ff9cc4ad1f3bf
diegochan25 <xdiego.chanx@gmail.com>
2026-03-01 22:37:51 -0500

    CRUD projects v0.1 (4/5)

---
+++ hq_listProjects/test/index.spec.ts
@@ -0,0 +1,12 @@
+/// <reference types="node">
+import  { describe, test, beforeEach } from "node:test";
+describe("hq_listProjects tests", () => {
+    beforeEach((c) => {
+        const ctx = c as TestContext;
+        
+    }
+
+    test("All in order", async (t) => {
+        
+    });
+});
